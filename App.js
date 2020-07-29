f6c9518719db8b04a025273707fd3708ae263008
shreyasnisal <33389081+shreyasnisal@users.noreply.github.com>
2020-07-30 04:08:17 +0530

    Added scheduled notifications

--- App.js
+++ App.js
@@ -9,10 +9,31 @@
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native'
 import Navigator from 'navigation/Navigator'
-import { StatusBar } from 'react-native'
+import { StatusBar, Platform } from 'react-native'
 import {Immersive} from 'react-native-immersive'
+import PushNotification from 'react-native-push-notification'
+
+PushNotification.configure({
+
+  onNotification: function (notification) {
+    // process the notification
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios',
+});
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    PushNotification.cancelAllLocalNotifications()
+  }
 
   componentDidMount() {
     Immersive.setImmersive(true)
@@ -21,6 +42,11 @@ export default class App extends Component {
 
   componentWillUnmount() {
     Immersive.removeImmersiveListener(this.restoreImmersive)
+
+    PushNotification.localNotificationSchedule({
+      message: "It's been a while since you played QLogic. Come solve some puzzles!",
+      date: new Date(Date.now() + 86400 * 7 * 1000),
+    });
   }
 
   restoreImmersive = () => {
